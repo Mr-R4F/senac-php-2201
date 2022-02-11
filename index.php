45fb8da20333fa82451bb95191bef77cd246b256
RAFAEL MARTINS SOARES <rafael.msoares5@senacsp.edu.br>
2022-02-10 21:22:27 -0300

    mais alteração

--- index.php
+++ index.php
@@ -4,3 +4,7 @@ echo "olá mundo! Que coisa mais legal";
 
 for ($i = 0; $i < 10; $i ++)
     echo "Um monte de olá mundo!<br>";
+
+ for ($i = 0; $i < 10; $i ++)
+    echo "Um monte de olá mundo!<br>";
+
